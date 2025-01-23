a32943e50c7e34ec5a90ee182a30267b012cc87e
Lama-Khaled-Ismail <lamakhaledismail@gmail.com>
2025-01-23 09:06:32 +0200

    organized client project structure

--- Client/src/main/java/iti/jets/client/HelloApplication.java
+++ Client/src/main/java/iti/jets/client/HelloApplication.java
@@ -6,13 +6,16 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("fxml/Login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+
+        scene.getStylesheets().add(Objects.requireNonNull(HelloApplication.class.getResource("styles/Login.css")).toExternalForm());
+        stage.setTitle("Log in");
         stage.setScene(scene);
         stage.show();
     }
