f4fe8d684dea71d11b36ff1f17f46bcf1d215e0c
Ahmed Ashraf <ahmedashraf8228@gmail.com>
2025-01-23 13:08:33 +0200

    edit

--- Client/src/main/java/gov/iti/jets/client/HelloApplication.java
+++ Client/src/main/java/gov/iti/jets/client/HelloApplication.java
@@ -1,4 +1,4 @@
-package iti.jets.client;
+package gov.iti.jets.client;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
--- Client/src/main/java/gov/iti/jets/client/HelloController.java
+++ Client/src/main/java/gov/iti/jets/client/HelloController.java
@@ -1,4 +1,4 @@
-package iti.jets.client;
+package gov.iti.jets.client;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
