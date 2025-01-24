fda986fe90ded8ba1ef0e33c41a455b969a73432
Hadeer Adel <119612208+Hadeer-Adel729@users.noreply.github.com>
2025-01-24 18:43:46 +0200

    Add files via upload

---
+++ Client/src/main/java/gov/iti/jets/client/allFriendController.java
@@ -0,0 +1,34 @@
+package gov.iti.jets.client;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Circle;
+
+public class allFriendController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ImageView friendIcon;
+
+    @FXML
+    private Circle status;
+
+    @FXML
+    private Label friendName;
+
+    @FXML
+    void initialize() {
+        assert friendIcon != null : "fx:id=\"friendIcon\" was not injected: check your FXML file 'allFriends.fxml'.";
+        assert status != null : "fx:id=\"status\" was not injected: check your FXML file 'allFriends.fxml'.";
+        assert friendName != null : "fx:id=\"friendName\" was not injected: check your FXML file 'allFriends.fxml'.";
+
+    }
+}
