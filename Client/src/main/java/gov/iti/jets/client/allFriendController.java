c73575494a24e3491b14ca145542789c8a267878
Hadeer Adel <119612208+Hadeer-Adel729@users.noreply.github.com>
2025-01-24 19:25:06 +0200

    Update allFriendController.java

--- Client/src/main/java/gov/iti/jets/client/allFriendController.java
+++ Client/src/main/java/gov/iti/jets/client/allFriendController.java
@@ -1,13 +1,12 @@
 package gov.iti.jets.client;
 
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.shape.Circle;
 
 public class allFriendController {
+
 
     @FXML
     private ImageView friendIcon;
@@ -19,14 +18,10 @@ public class allFriendController {
     private Label friendName;
 
     @FXML
-    private ImageView inbox;
-
-    @FXML
     void initialize() {
         assert friendIcon != null : "fx:id=\"friendIcon\" was not injected: check your FXML file 'allFriends.fxml'.";
         assert status != null : "fx:id=\"status\" was not injected: check your FXML file 'allFriends.fxml'.";
         assert friendName != null : "fx:id=\"friendName\" was not injected: check your FXML file 'allFriends.fxml'.";
-        assert inbox != null : "fx:id=\"inbox\" was not injected: check your FXML file 'allFriends.fxml'.";
 
     }
 }
