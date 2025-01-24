51911550b6d1ccd599f1a1fbdcaf37ed7f90a519
Hadeer Adel <119612208+Hadeer-Adel729@users.noreply.github.com>
2025-01-24 18:40:20 +0200

    Update and rename pendingController.java to onlineFriendController.java

--- Client/src/main/java/gov/iti/jets/client/onlineFriendController.java
+++ Client/src/main/java/gov/iti/jets/client/onlineFriendController.java
@@ -7,7 +7,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.shape.Circle;
 
-public class pendingController {
+public class onlineFriendController {
 
     @FXML
     private ResourceBundle resources;
