611a02e8a111be097a17e38b8f5d3f692b263c81
Ahmed Ashraf <ahmedashraf8228@gmail.com>
2025-01-24 16:39:55 +0200

    update home

--- Client/src/main/java/gov/iti/jets/client/HelloApplication.java
+++ Client/src/main/java/gov/iti/jets/client/HelloApplication.java
@@ -11,12 +11,14 @@ import java.util.Objects;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("fxml/Login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("fxml/home.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
 
-        scene.getStylesheets().add(Objects.requireNonNull(HelloApplication.class.getResource("styles/Login.css")).toExternalForm());
+        scene.getStylesheets().add(Objects.requireNonNull(HelloApplication.class.getResource("styles/home.css")).toExternalForm());
         stage.setTitle("Log in");
         stage.setScene(scene);
+        stage.setMinHeight(600);
+        stage.setMinWidth(1000);
         stage.show();
     }
 
