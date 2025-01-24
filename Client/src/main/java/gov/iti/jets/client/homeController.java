0aec674fbf88320b7c5c9eae79a1d3e79664796b
Ahmed Ashraf <ahmedashraf8228@gmail.com>
2025-01-24 17:37:47 +0200

    update class name

--- Client/src/main/java/gov/iti/jets/client/homeController.java
+++ Client/src/main/java/gov/iti/jets/client/homeController.java
@@ -2,17 +2,22 @@ package gov.iti.jets.client;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
-public class homeController {
+public class HomeController {
 
     @FXML
     private ResourceBundle resources;
 
     @FXML
     private URL location;
+
+    @FXML
+    private Button allButton;
 
     @FXML
     private BorderPane homeBorderPane;
@@ -24,7 +29,13 @@ public class homeController {
     private AnchorPane stackpane;
 
     @FXML
+    void allButtonHandling(ActionEvent event) {
+
+    }
+
+    @FXML
     void initialize() {
+        assert allButton != null : "fx:id=\"allButton\" was not injected: check your FXML file 'home.fxml'.";
         assert homeBorderPane != null : "fx:id=\"homeBorderPane\" was not injected: check your FXML file 'home.fxml'.";
         assert left_pane != null : "fx:id=\"left_pane\" was not injected: check your FXML file 'home.fxml'.";
         assert stackpane != null : "fx:id=\"stackpane\" was not injected: check your FXML file 'home.fxml'.";
