94dc714feb66d53b465f3bb5a377327736bb6cbb
Ahmed Shabaan <adelhadeer436@gmail.com>
2025-01-27 15:59:48 +0200

    update chatArea scene

--- Client/src/main/java/gov/iti/jets/client/controller/ChatAreaController.java
+++ Client/src/main/java/gov/iti/jets/client/controller/ChatAreaController.java
@@ -1,63 +1,132 @@
 package gov.iti.jets.client.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 
 public class ChatAreaController {
+
     @FXML
-    private ImageView send;
+    private Button attachment;
+
+    @FXML
+    private Button block;
+
+    @FXML
+    private Button call;
+
+    @FXML
+    private Button camera;
+
+    @FXML
+    private Button emoji;
+
+    @FXML
+    private ImageView friendIcon;
+
+    @FXML
+    private Label friendName;
+
     @FXML
     private TextField messageField;
-    @FXML
-    private ImageView attachment;
-    @FXML
-    private ImageView mic;
-    @FXML
-    private ImageView emoji;
-    @FXML
-    private ImageView camera;
-    @FXML
-    private ImageView user;
-    @FXML
-    private ImageView call;
-    @FXML
-    private ImageView video;
 
     @FXML
-    public void sendMessage(MouseEvent mouseEvent) {
+    private Button mic;
+
+    @FXML
+    private Button send;
+
+    @FXML
+    private Button video;
+
+    @FXML
+    void BlockFriendAction(ActionEvent event) {
 
     }
 
     @FXML
-    public void recordVoice(MouseEvent mouseEvent) {
+    void callAction(ActionEvent event) {
+
     }
 
     @FXML
-    public void sendMessageByKey(KeyEvent keyEvent) {
+    void camerAction(ActionEvent event) {
+
     }
 
     @FXML
-    public void sendEmoji(MouseEvent mouseEvent) {
+    void micAction(ActionEvent event) {
+
     }
 
     @FXML
-    public void sendVideo(MouseEvent mouseEvent) {
+    void recordVoice(MouseEvent event) {
+
     }
 
     @FXML
-    public void sendAttachment(MouseEvent mouseEvent) {
+    void sendAction(ActionEvent event) {
+
     }
 
     @FXML
-    public void showUserInfo(MouseEvent mouseEvent) {
+    void sendAttachment(MouseEvent event) {
+
     }
+
     @FXML
-    public void voiceCall(MouseEvent mouseEvent) {
+    void sendEmoji(MouseEvent event) {
+
     }
+
     @FXML
-    public void videoCall(MouseEvent mouseEvent) {
+    void sendEmojiAction(ActionEvent event) {
+
     }
+
+    @FXML
+    void sendFiles(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sendMessage(MouseEvent event) {
+
+    }
+
+    @FXML
+    void sendMessageByKey(KeyEvent event) {
+
+    }
+
+    @FXML
+    void sendVideo(MouseEvent event) {
+
+    }
+
+    @FXML
+    void showUserInfo(MouseEvent event) {
+
+    }
+
+    @FXML
+    void videoCall(MouseEvent event) {
+
+    }
+
+    @FXML
+    void viodeoAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void voiceCall(MouseEvent event) {
+
+    }
+
 }
