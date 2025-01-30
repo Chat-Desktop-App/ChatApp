6f6b82966f10e183956e9e56155eb9084e1d6138
Ahmed Ashraf <ahmedashraf8228@gmail.com>
2025-01-30 23:31:19 +0200

    aaa

---
+++ Client/src/main/java/gov/iti/jets/HelloApplication.java
@@ -0,0 +1,27 @@
+package gov.iti.jets;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("fxml/home.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+
+        //scene.getStylesheets().add(Objects.requireNonNull(HelloApplication.class.getResource("styles/login.css")).toExternalForm());
+        stage.setTitle("Log in");
+        stage.setMinHeight(550);
+        stage.setMinWidth(1050);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
