426e0404bd9f2e750e54916220335eeed04eca02
Lama-Khaled-Ismail <lamakhaledismail@gmail.com>
2025-01-30 16:43:50 +0200

    added login service in serverside

---
+++ Server/src/main/java/gov/iti/jets/RMIConnector.java
@@ -0,0 +1,28 @@
+package gov.iti.jets;
+
+import gov.iti.jets.services.impls.LoginImpl;
+
+import java.rmi.AccessException;
+import java.rmi.AlreadyBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class RMIConnector {
+    public RMIConnector(){
+        try {
+            LoginImpl login = new LoginImpl();
+            Registry registry = LocateRegistry.createRegistry(1099);
+            registry.bind("LogIn", login);
+        } catch (RemoteException e) {
+            throw new RuntimeException(e);
+        } catch (AlreadyBoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args){
+        RMIConnector rmi = new RMIConnector();
+    }
+
+}
