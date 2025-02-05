d51d92682a3064407113908a5e61b617250cdb31
Lama-Khaled-Ismail <lamakhaledismail@gmail.com>
2025-02-05 19:17:41 +0200

    email doesn't have to be unique

--- DB Schema/chat_app_DDL.sql
+++ DB Schema/chat_app_DDL.sql
@@ -227,7 +227,6 @@ CREATE TABLE `users` (
   `Last_seen` datetime NOT NULL,
   `is_admin` tinyint(1) NOT NULL,
   PRIMARY KEY (`phone_number`),
-  UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
