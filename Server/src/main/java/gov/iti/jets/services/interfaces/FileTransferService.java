e951b3daaf29bb40d3278f53e2e31a47a7569358
Yusuf-Salah-ElDin <yusuf_mustafa1553@outlook.com>
2025-02-08 16:57:18 +0200

    file transfer service

--- Server/src/main/java/gov/iti/jets/services/interfaces/FileTransferService.java
+++ Server/src/main/java/gov/iti/jets/services/interfaces/FileTransferService.java
@@ -1,8 +1,9 @@
 package gov.iti.jets.services.interfaces;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface FileTransferService extends Remote {
-
-
+    boolean uploadFile (byte[] fileData, String fileName, String fileTypr) throws RemoteException;
+    byte[] downloadFile (int fileId) throws RemoteException;
 }
