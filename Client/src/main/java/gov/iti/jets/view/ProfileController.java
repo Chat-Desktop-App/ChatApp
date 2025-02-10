f6c02c54caa7de65e28a39e964a1778456036fcc
Hadeer Adel <119612208+Hadeer-Adel729@users.noreply.github.com>
2025-02-10 13:12:20 +0200

    update profile

--- Client/src/main/java/gov/iti/jets/view/ProfileController.java
+++ Client/src/main/java/gov/iti/jets/view/ProfileController.java
@@ -5,6 +5,12 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+import java.io.IOException;
 
 public class ProfileController {
 
@@ -27,13 +33,29 @@ public class ProfileController {
     private ComboBox<?> status;
 
     @FXML
+    private ImageView picId;
+
+    @FXML
     void handleEditButton(ActionEvent event) {
 
     }
 
     @FXML
-    void handleProfilePic(ActionEvent event) {
+    void handleProfilePic(ActionEvent event) throws IOException {
+            FileChooser chooser = new FileChooser();
+            chooser.setTitle("Open File");
+            chooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg", "*.gif", "*.bmp"));
+            File file = chooser.showOpenDialog(picId.getScene().getWindow()); // Use the current window
+            if(file != null) {
+                String imagepath = file.toURI().toString();
 
+                Image image = new Image(imagepath);
+                picId.setImage(image);
+            }
+            else
+            {
+               picId = picId;
+            }
     }
 
 }
